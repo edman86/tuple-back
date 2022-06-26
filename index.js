862881ff46f02fd1a7dc59abe54d1cb1d98fca04
edman <compFoxLord007@gmail.com>
2022-06-26 16:27:12 +0400

    Added auth for users

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { router as usersRouter } from './routes/usersRouter.js';
 
-const usersRouter = require('./routes/users');
-const announcementsRouter = require('./routes/announcements');
+// const announcementsRouter = require('./routes/announcements');
  
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,19 +13,16 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true });
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Connection success');
-});
+mongoose.connect(uri)
+    .then(() => console.log('Connection success'))
+    .catch((err) => console.log('Error', err));
 
 app.get('/', (req, res) => {
     res.send('Yo dude!')
 });
 
 app.use('/users', usersRouter);
-app.use('/announcements', announcementsRouter);
+// app.use('/announcements', announcementsRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
